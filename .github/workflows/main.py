1aec5e2c46f7d047a78f5af1fd5228757d80ce35
zx-yk <112702785+zx-yk@users.noreply.github.com>
2022-09-04 22:43:19 +0800

    Update main.py

--- .github/workflows/main.py
+++ .github/workflows/main.py
@@ -85,8 +85,8 @@ if __name__ == '__main__':
     appId = 'wx0ba8ab3d5dd8ec0d'
     appSecret = '4b3d6cbf754ba8e57c3c63a8e4f8e294'
     # 要发送人的openId列表
-    # openIdList = ['ofngy51wcjExzAiFlLCLOrTYIiG8','ofngy54YIdPePMHO7MWqBN3krPBQ']
-    openIdList = ['ofngy51wcjExzAiFlLCLOrTYIiG8']
+    openIdList = ['ofngy51wcjExzAiFlLCLOrTYIiG8','ofngy54YIdPePMHO7MWqBN3krPBQ']
+    # openIdList = ['ofngy51wcjExzAiFlLCLOrTYIiG8']
     # 模版Id
     templateId = 'EC4ThFNAtVf1bzCKi30TW92heI1Cibgn0-v626lPZsM'
     # 高德天气API key
